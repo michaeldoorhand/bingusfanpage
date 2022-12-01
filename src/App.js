e4c2c129f4df04e9b2f4b36bd28e378ec2084313
Michael Durand <michael.durand45@gmail.com>
2022-11-30 19:26:53 -0600

    mobile fix + twitter feed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import galleryPage from "./Gallerypage";
 import testpage from "./Testpage";
 import ScrollToTop from "./ScrollToTop";
 import aboutbing from "./images/aboutbing.jpg";
+import { Helmet } from "react-helmet";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
 
 function sayHello() {
   alert("You clicked me!");
@@ -26,74 +28,77 @@ function App() {
 
   return (
     <div className="background">
-      <div className="home-container">
-        <div className="welcome-text-container">
-          <div>
-            <h1 className="welcome-text">
-              Welcome to the official bingus fan page!
-            </h1>
-          </div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>OFFICIAL Bingus Fan Page</title>
+      </Helmet>
+      <div className="welcome-text-container">
+        <h1 className="welcome-text">
+          Welcome to the official <br /> bingus fan page!
+        </h1>
+      </div>
+      <div className="row">
+        <div className="yobing">
+          <img src={yobing} alt="wait until the page loads" className="flex" />
         </div>
-
-        <div className="bingus-container">
-          <div className="bingus">
-            <img
-              src={yobing}
-              alt="wait until the page loads"
-              className="flex"
-            />
-          </div>
-
-          <div className="bingus">
-            <img
-              onClick={() => callMySound(soundSrc)}
-              className="flex"
-              src={img1}
-              alt=""
-            />
-          </div>
-
-          <div className="bingus">
-            <img
-              src={yobing}
-              alt="wait until the page loads"
-              className="flex reverse"
-            />
-          </div>
+        <div className="yobing">
+          <img
+            onClick={() => callMySound(soundSrc)}
+            src={img1}
+            alt=""
+            className="flex"
+          />
         </div>
-        <ScrollToTop>
-          <div>
-            <Link to="/gallerypage" className="header-text">
-              Click me to go to the official bingus gallery!
-            </Link>
-            <br />
-            <Link to="/otters" className="header-text">
-              {" "}
-              What is bingus' favorite animal?
-            </Link>
-          </div>
-        </ScrollToTop>
-
-        <h1 className="about-bing-header">Meet the man, the myth, the Bing:</h1>
-        <div className="about-container">
-          <img src={aboutbing} alt="" className="aboutimg"></img>
-          <div className="about-text-container">
-            <h2 className="about-txt">
-              &nbsp;&nbsp;&nbsp;&nbsp;Hello! My name is Pollo, but you probably
-              know me as bingus. I am a munchkin sphynx cat who enjoys lounging
-              around and being a silly boy. Some of my favorite activities
-              include knocking over glasses, chasing string, and making biscuits
-              on the lap of this websites creator. As far as my ambitions go, my
-              dream in life is to aid and abet in the overthrow of the U.S.
-              government. My secondary goal in life is to release my Latin
-              claymation love story "Lorem Ipsum" to Bollywood. Overall im just
-              your average bing trying to find his way in life. If you're
-              interested to hear more about me or to follow my progress with
-              these goals feel free to follow my blog "Word on the bing." I hope
-              you enjoy my site!
-            </h2>
-          </div>
+        <div className="yobing">
+          <img
+            src={yobing}
+            alt="wait until the page loads"
+            className="reverse"
+          />
         </div>
+      </div>
+      <ScrollToTop>
+        <Link to="/gallerypage" className="header">
+          Click me to go to the official bingus gallery!
+        </Link>
+
+        <Link to="/otters" className="header">
+          {" "}
+          What is bingus' favorite animal?
+        </Link>
+      </ScrollToTop>
+      <h1 className="header">Meet the man, the myth, the Bing:</h1>
+      <div className="about-section">
+        <img src={aboutbing} alt="" className="about-img"></img>
+
+        <h2 className="about-txt">
+          &nbsp;&nbsp;&nbsp;&nbsp;Hello! My name is Pollo, but you probably know
+          me as bingus. I am a munchkin sphynx cat who enjoys lounging around
+          and being a silly boy. Some of my favorite activities include knocking
+          over glasses, chasing string, and making biscuits on the lap of this
+          websites creator. As far as my ambitions go, my dream in life is to
+          aid and abet in the overthrow of the U.S. government. My secondary
+          goal in life is to release my Latin claymation love story "Lorem
+          Ipsum" to Bollywood. Overall im just your average bing trying to find
+          his way in life. If you're interested to hear more about me or to
+          follow my progress with these goals feel free to follow my blog "Word
+          on the bing." I hope you enjoy my site!
+        </h2>
+      </div>
+      <div className="twitter">
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="dailycatpics"
+          options={{ height: 800 }}
+        />
+      </div>
+
+      <div className="footer">
+        website icon by Rohim:&nbsp;{" "}
+        <a target="_blank" href="https://www.flaticon.com/authors/rohim">
+          {" "}
+          https://www.flaticon.com/authors/rohim
+        </a>
       </div>
     </div>
   );
--- src/components/OtterCamComponent.js
+++ src/components/OtterCamComponent.js
@@ -1,15 +1,17 @@
 const OtterCamComponent = () => {
   return (
     <div className="background">
-      <iframe
-        width="1000"
-        height="600"
-        src="https://www.youtube.com/embed/JQnxefImhu8?controls=1"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+      <div className="otter-container">
+        <iframe
+          width="1000"
+          height="600"
+          src="https://www.youtube.com/embed/JQnxefImhu8?controls=1"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
   );
 };
